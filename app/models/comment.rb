80cbef916602ee3efd3bc6750bfac0ad1888025f
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-20 14:31:21 -0500

    Add arguments into the constructor and modified the id filter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,14 @@ class Comment < ApplicationRecord
   belongs_to :user, class_name: 'User'
   belongs_to :post, class_name: 'Post'
 
-  def initialize
-  	update_posts_counter
+  def initialize(*args)
+  	super(*args)
+  	update_comments_counter if args.any? # Executes the method only if an argument is provided.
   end
 
   # Method to update the comments counter for a post.
-  def update_posts_counter
-  	post = Post.find_by(id: :post)
+  def update_comments_counter
+  	post = Post.find_by(id: post_id)
   	if post.comments_counter.nil?
       counter = 1
     else
