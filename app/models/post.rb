cdcbfbc5b74ba9595c6e2fe363fb6ea8f141d975
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-20 13:46:26 -0500

    Created a method to update the posts for a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,19 @@ class Post < ApplicationRecord
 	has_many :comments
 	has_many :likes
   belongs_to :author, class_name: "User"
+
+  def initialize
+		update_posts
+  end
+
+  # Method to update the posts_counter for a user.
+  def update_posts
+  	user = User.find_by(id: :author)
+  	if user.posts_counter.nil?
+  		counter = 1
+  	else
+  		counter = user.posts_counter + 1
+  	end
+  	user.update(posts_counter: counter)
+  end
 end
