b90b6e71ebd72c4c74f114cb548bb0ac9ad8f670
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-20 13:55:40 -0500

    Created a method to update the likes counter for a post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,19 @@
 class Like < ApplicationRecord
   belongs_to :user, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
+  def initialize
+    update_likes_counter
+   end
+
+  # Method to update the likes counter for a post.
+  def update_likes_counter
+   	post = Post.find_by(id: :post)
+    if post.likes_counter.nil?
+       counter = 1
+    else
+       counter = post.likes_counter + 1
+    end
+    post.update(likes_counter: counter)
+  end
 end
