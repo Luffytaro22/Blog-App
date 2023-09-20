5fe045b95cf68dbf90a89c7788e827f617a1a6b7
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-20 13:35:01 -0500

    Add a recent_posts method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
 	has_many :posts
 	has_many :comments
 	has_many :likes
+
+	# Method to return the 3 most recent posts for a given user.
+	def recent_posts(author_id)
+		Post
+			.where(author_id: author_id)
+			.order(updated_at: :desc)
+			.limit(3)
+	end
 end
