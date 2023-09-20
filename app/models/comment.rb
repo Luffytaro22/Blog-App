40fde25593e115695d81b8927ae35294f951fc1a
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-20 13:53:44 -0500

    Created a method to update the comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,19 @@
 class Comment < ApplicationRecord
   belongs_to :user, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
+  def initialize
+  	update_posts_counter
+  end
+
+  # Method to update the comments counter for a post.
+  def update_posts_counter
+  	post = Post.find_by(id: :post)
+  	if post.comments_counter.nil?
+      counter = 1
+    else
+     	counter = post.comments_counter + 1
+    end
+    post.update(comments_counter: counter)
+  end
 end
