70fe367d85fc5cca50298e35096c728e95375ce5
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-21 10:44:21 -0500

    Added validations to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+  validates :user, presence: true # Name must not be blank.
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
 
   # Method to return the 3 most recent posts for a given user.
   def recent_posts
