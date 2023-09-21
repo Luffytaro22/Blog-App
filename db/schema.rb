569062d5a3e2171de4de9aa4ef6446cab5e8b25e
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-21 11:39:17 -0500

    Revert "Update schema"
    This reverts commit 92d272500a56f27ea250b9937af6b21b2569a5bc.

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_201156) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter", default: 0
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
