f375551c4e5f3812717f5edc01777081af707d3c
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-21 11:29:57 -0500

    Change a typo error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
-  validates :user, presence: true # Name must not be blank.
+  validates :name, presence: true # Name must not be blank.
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
   # Method to return the 3 most recent posts for a given user.
