9e508d2588c547f09cc9e563137b7c492e402859
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-21 11:40:13 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_201156) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
