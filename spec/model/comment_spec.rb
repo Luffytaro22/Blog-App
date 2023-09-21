cce030a7f5c7bceb7e8caa41bfd648c9b22fe1ee
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-21 14:53:46 -0500

    Add tests to check the comments counter

--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -1,15 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :module do
-		user = User.new(name: 'John', photo: 'www.google.com', bio: 'My name is...', id: 1)
-		post = Post.new(author: user, title: 'My life', text: "It's my life...", id: 2)
-		comment = Comment.new(user: user, post: post, text: 'Very funny!')
+		let(:user) { User.new(name: 'John', photo: 'www.google.com', bio: 'My name is...') }
+		let(:post) { Post.new(author: user, title: 'My life', text: "It's my life...") }
+		let(:comment) { Comment.new(user: user, post: post, text: 'Very funny!') }
+
+	context 'After saving the user and post' do
+		before do
+			user.save
+			post.save
+			comment.save
+		end
 
 		it 'Should reference the user id' do
-			expect(comment.user_id).to eq(1)
+			expect(comment.user_id).to eq(user.id)
 		end
 
 		it 'Should reference the post id' do
-			expect(comment.post_id).to eq(2)
+			expect(comment.post_id).to eq(post.id)
 		end
+
+		it 'The comments counter should be 1' do
+			post.reload
+			expect(post.comments_counter).to eq(1)
+		end
+
+		after do
+			user.reload
+			post.reload
+			comment.destroy
+			post.destroy
+			user.destroy
+		end
+	end
 end
