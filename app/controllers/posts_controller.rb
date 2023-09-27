be128510f15291d2d595ad6006d1b29ffa8f0c94
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-27 12:24:39 -0500

    Modify the searching to return all the posts for an author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def show
     @id = params[:id]
     @user_select = User.find(params[:user_id])
-    @post = @user_select.recent_posts[@id.to_i - 1]
+    @post = Post.where(author_id: @user_select.id)[@id.to_i - 1]
     @comments = Comment.where(post_id: @post.id)
   end
 end
