42e9d3658df9299df5a408efd058bf380f8eb852
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-27 18:29:28 -0500

    Add a @posts variable to save all the posts for an author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
   def show
     @id = params[:id]
     @user_select = User.find(params[:user_id])
-    @post = Post.where(author_id: @user_select.id)[@id.to_i - 1]
+    @posts = Post.where(author_id: @user_select.id)
+    @post = @posts[@id.to_i - 1]
     @comments = Comment.where(post_id: @post.id)
   end
 end
