c29d5a25da03dc6d48aeb193a866edc91623c2c6
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-02 13:00:30 -0500

    Use the new method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user_select = User.find(params[:user_id])
-    @posts = Post.includes(:author).where(author_id: params[:user_id])
+    @posts = Post.with_recent_comments(params[:user_id])
     @user = current_user
   end
 
