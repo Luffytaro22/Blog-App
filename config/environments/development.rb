1b730526be1bb6eabaaa28dbbfa9636a0257b814
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-04 12:40:04 -0500

    Added configurations to send the confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,17 @@ Rails.application.configure do
 
 	config.action_mailer.perform_caching = false
 
-	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+	config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      user_name:      ENV['SENDMAIL_USERNAME'],
+      password:       ENV['SENDMAIL_PASSWORD'],
+      domain:         ENV['MAIL_HOST'],
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
