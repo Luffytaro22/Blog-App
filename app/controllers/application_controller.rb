e0c5458b8937dc11bfdda4b2748ea62d194b2f5d
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-04 12:14:30 -0500

    Defined an after_sign_out_path_for method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :photo, :bio, :password, :current_password)}
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
   end
 end
