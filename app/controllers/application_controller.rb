c6e736557808ff211a141650cc9a144f5a8f561b
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-04 19:19:55 -0500

    Fixed linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,18 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :photo, :bio, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :photo, :bio, :password, :current_password)
+    end
   end
 
-  def after_sign_out_path_for(resource_or_scope)
-      new_user_session_path
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
   end
 end
