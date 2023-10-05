be5ec316ced4f63167037a9b8d3dc9059f1b571a
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-05 10:54:09 -0500

    Created a method to decrease the posts counter when it'Âs destroyed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   after_initialize :set_defaults
   after_create :update_posts
+  after_destroy :decrease_posts_counter
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
@@ -18,6 +19,11 @@ class Post < ApplicationRecord
   def update_posts
     user = User.find_by(id: author_id)
     user.increment!(:posts_counter)
+  end
+
+  def decrease_posts_counter
+  	user = User.find_by(id: author_id)
+    user.decrement!(:posts_counter)
   end
 
   # Method to return the 5 most recent comments for a post.
