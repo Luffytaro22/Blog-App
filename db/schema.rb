4d3ac813a0bb51e6f0e175f1e80d70fc6ac65098
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-05 10:11:34 -0500

    Update the schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_152605) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_150808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_152605) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.string "role", default: "default"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
