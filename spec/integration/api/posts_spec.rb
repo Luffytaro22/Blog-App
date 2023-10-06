591bb995af13877b26b555d9f49711523a936e34
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-06 16:02:52 -0500

    Added tests to the apis specs

--- spec/integration/api/posts_spec.rb
+++ spec/integration/api/posts_spec.rb
@@ -1,4 +1,20 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/posts', type: :request do
+	path '/api/v1/users/{user_id}/posts' do
+      parameter name: :user_id, in: :path, type: :integer
+
+      get 'Retrieves posts of a user' do
+        produces 'application/json'
+        let(:user) { create(:user) }
+        let(:user_id) { user.id }
+
+        response '200', 'posts retrieved' do
+          schema type: :array,
+            items: { '$ref' => '#/definitions/post' }
+
+          run_test!
+        end
+      end
+  end
 end
