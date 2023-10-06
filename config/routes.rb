a6dd4739c69c8ece85b56c74cb83a5470c46e0d6
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-06 16:24:00 -0500

    Added routes to the /api-docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	mount Rswag::Ui::Engine => '/api-docs'
+	mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 	devise_for :users
   # Defines the root path route ("/")
