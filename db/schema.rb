bb387a640fa8b2948aba6466268ed30d154dbd73
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-27 22:57:10 -0500

    Remove email confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -56,13 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_150808) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
     t.string "role", default: "default"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
