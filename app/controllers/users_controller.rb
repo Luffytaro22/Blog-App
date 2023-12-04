11ed22f3867abf3ca123fca3a19e9efa6983c94f
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-12-04 08:24:29 -0500

    Improve users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
+  before_action :find_user
   load_and_authorize_resource
+
   def index
     @users = User.all
   end
 
   def show
     @user_select = User.find(params[:id])
+  end
+
+  private
+
+  def find_user
     @user = current_user
   end
 end
